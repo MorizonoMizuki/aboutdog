bef745491ad3e4e8c4dbb3c830c72f64f76cafe8
森薗美月 <attesa.1.mzk@gmail.com>
2020-06-10 16:38:42 +0900

    no message

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/leset.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+    <?php wp_head(); ?>
+</head>
+
+<body>
+  <div class="container">
+        <header>
+
+    
+            <h1>About Dog</h1>
+            <nav>
+                <ul class="hader-right">
+                    <li>TOP</li>
+                   <li>Latest</li>
+                   <li>Contact</li>
+                </ul>
+            </nav>
+       
+
+        </header>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+        <section class="dog-image"></section>
+
+        <section>
+
+           <h2 class="sub-title">Latest</h2>
+
+            <ul class="aryicles">
+
+
+             <li class="article">
+                 <img class="article-img" src="<?php echo get_template_directory_uri(); ?>/img/dog (1).jpg" alt="dog6">
+                 <p class="artycle-content">ここに記事が表示される</p>
+                 <p class="artycle-content">ここに記事が表示される</p>
+                 <p class="read-more"><a href="">Read More</a></p>
+             </li>
+
+            </ul>
+        
+        </section>
+  </div>
+  <?php wp_footer(); ?>
+</body>
+</html>
